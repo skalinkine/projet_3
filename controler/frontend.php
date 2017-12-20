dc630aa63046265b05f5001618faddb45b8ad407
Stéphanie MARTIN <s.kalinkine@formalearning.com>
2017-12-20 11:42:48 +0100

    mise à jour du controler frontend pour ajout des fonctions qui traitent les commentaires

--- controler/frontend.php
+++ controler/frontend.php
@@ -1,12 +1,12 @@
 <?php
 
-// Chargement des classes
 require_once('model/PostManager.php');
+require_once('model/CommentManager.php');
 
 function listPosts()
 {
-    $postManager = new PostManager(); // Création d'un objet instance de la classe PostManager
-    $posts = $postManager->getPosts(); // Appel d'une fonction de cet objet
+    $postManager = new PostManager(); 
+    $posts = $postManager->getPosts(); 
 
     require('view/frontend/listPostsView.php');
 }
@@ -14,7 +14,47 @@ function listPosts()
 function post()
 {
     $postManager = new PostManager();
-    $post = $postManager->getPost($_GET['id']);
+    $commentManager = new CommentManager();
 
+    $post = $postManager->getPost($_GET['id']);
+    $comments = $commentManager->getComments($_GET['id']);
+
+    require('view/frontend/postView.php');
+}
+
+function addComment($postId, $author, $comment)
+{
+    $commentManager = new CommentManager();
+
+    $affectedLines = $commentManager->postComment($postId, $author, $comment);
+
+    if ($affectedLines === false) {
+        throw new Exception('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $postId);
+    }
+}
+
+function modifyComment($postId, $commentId, $author=null, $comment=null)
+{
+    $postManager = new PostManager();
+    $commentManager = new CommentManager();
+
+    if ($author != null && $comment != null) {
+        $affectedLines = $commentManager->updateComment($commentId, $author, $comment);
+
+        if ($affectedLines === false) {
+        throw new Exception('Impossible de modifier le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $postId);
+    }
+    }
+    
+
+    $post = $postManager->getPost($postId);
+    $comments = $commentManager->getComments($postId);
+    
     require('view/frontend/postView.php');
 }
